0347f5b4c4a0a144ab39aa9b2d230679e1138476
DigitOtter <digitotter@protonmail.com>
2024-02-12 04:32:59 +0100

    Add VK_CHECK macro

--- gd_texture_share_vk/rendering_backend.hpp
+++ gd_texture_share_vk/rendering_backend.hpp
@@ -9,6 +9,7 @@ using texture_id_t           = GLuint;
 using texture_format_t       = GLuint;
 
 #else
+#include <iostream>
 #include <texture_share_vk/texture_share_vk_client.hpp>
 #include <texture_share_vk/texture_share_vk_setup.hpp>
 
@@ -16,6 +17,16 @@ using texture_share_client_t = TextureShareVkClient;
 using texture_id_t           = VkImage;
 using texture_format_t       = VkFormat;
 
+#define VK_CHECK(x)                                                                    \
+	do                                                                             \
+	{                                                                              \
+		VkResult err = x;                                                      \
+		if(err)                                                                \
+		{                                                                      \
+			std::cout << "Detected Vulkan error: " << err << std::endl;    \
+			abort();                                                       \
+		}                                                                      \
+	} while(0)
 #endif
 
 using tsv_image_format_t     = ImgFormat;
